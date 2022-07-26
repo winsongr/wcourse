bb8bb0644631e70d86dc93d5939c7e04c7a39e22
Winson G <96694861+winsongr@users.noreply.github.com>
2022-07-26 10:29:12 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/winson/wcourse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.winson.wcourse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    GetMaterialApp(
+      title: "Wcourse",
+      initialRoute: AppPages.INITIAL,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      getPages: AppPages.routes,
+    ),
+  );
+}
